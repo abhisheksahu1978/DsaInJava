7820c6fbd0115488455466ffc8b82d8a01c08a0f
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2026-08-31 13:50:44 +0530

    Add More Java Dsa Program

---
+++ 02 - String/LowerCaseString.java
@@ -0,0 +1,9 @@
+public class LowerCaseString{
+    public static void main(String[] args){
+        String name = "ABHISHEK KUMAR";
+
+        String s = name.toLowerCase();
+
+        System.out.print(s);
+    }
+}
---
+++ 02 - String/UppercaseString.java
@@ -0,0 +1,9 @@
+public class UppercaseString{
+    public static void main(String[] args){
+        String name = "abhishek";
+
+        String s = name.toUpperCase();
+
+        System.out.print(s);
+    }
+}
