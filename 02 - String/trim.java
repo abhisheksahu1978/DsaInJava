339dd9de0eab35b6fe9aec12fba6a21ab580e6cd
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2026-09-02 13:19:44 +0530

    Add More Java Dsa Program

---
+++ 02 - String/trim.java
@@ -0,0 +1,11 @@
+public class trim{
+    public static void main (String[] args){
+        // Clear extra spaces
+        // String intro = "     I am a normal engineering student.      ";
+        // String introString = intro.trim();
+
+        // System.out.print(introString.trim());
+        String nonItroString = "        Abhishek      ";
+        System.out.print(nonItroString);
+    }
+}
